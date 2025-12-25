9a8a573622b7a737b8f3c1ec6b1ca1dc9f94c932
Venkat-2832 <a.venkatsai1234@example.com>
2025-12-25 12:41:01 +0530

    adding more files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <h1>My Profile Website</h1>
+      <Skills />
     </div>
   );
 }
